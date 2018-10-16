f2ce0d2d7c33d978d84e4dffefe05ea440199aad
Thiago sobrinho <thiago122@hotmail.com>
2018-10-16 15:50:41 -0300

    inicial

---
+++ vaga.js
@@ -0,0 +1,33 @@
+(function(){
+
+    var atividades = [
+        'Gerenciamento de conteúdo em sites',
+        'Criação de e-mail marketing',
+        'Ajustes em sites'
+    ];
+
+    var requisitos = [
+        'HTML básico',
+        'Conhecimentos em WordPress',
+        'Conhecimentos básicos de Photoshop',
+        'Organização',
+        'Proatividade'
+    ];
+
+    var formacao = [
+        'Comunicação em Ciências da Computação',
+        'Análises de Sistemas',
+        'Tecnologia da Informação',
+        'Engenharia da Computação',
+        'Outras relacionadas'
+    ];
+
+    var diferenciais = [
+        'Conhecimentos gerais no pacote Adobe'
+    ];
+
+    var contato = ['r','b','.','m','o','c','.','o','a','c','a','c','i','n','u','m','o','c','x','v','@','e','l','a','f'];
+    contato.reverse()
+    console.log('Envie seu currículo com o assunto "Estágio de desenvolvimento" para: ' + contato.join(''));
+
+})();
